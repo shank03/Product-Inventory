afd1439e5c2842d57cdfd3e8bb599439154850c7
Shashank Verma <shashank.verma2002@gmail.com>
2023-12-18 12:01:45 +0530

    init: Populate mock data
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- main.go
+++ main.go
@@ -29,7 +29,59 @@ type Product struct {
 	Type ProductType
 }
 
+var productList = map[int]Product{}
 var inventory = map[ProductType]Inventory{}
+
+func init() {
+	// populate product list
+	productList[1] = Product{
+		ID:   1,
+		Name: "Coke",
+		Type: drinks,
+	}
+	productList[3] = Product{
+		ID:   3,
+		Name: "Sprite",
+		Type: drinks,
+	}
+	productList[4] = Product{
+		ID:   4,
+		Name: "Lays",
+		Type: food,
+	}
+	productList[6] = Product{
+		ID:   6,
+		Name: "Cheetos",
+		Type: food,
+	}
+	productList[7] = Product{
+		ID:   7,
+		Name: "Toilet Paper",
+		Type: utils,
+	}
+
+	// populate inventory
+	inventory[drinks] = Inventory{
+		Type: drinks,
+		IDs: map[int]Product{
+			1: productList[1],
+			3: productList[3],
+		},
+	}
+	inventory[food] = Inventory{
+		Type: food,
+		IDs: map[int]Product{
+			4: productList[4],
+			6: productList[6],
+		},
+	}
+	inventory[utils] = Inventory{
+		Type: utils,
+		IDs: map[int]Product{
+			7: productList[7],
+		},
+	}
+}
 
 func main() {
 	e := echo.New()
